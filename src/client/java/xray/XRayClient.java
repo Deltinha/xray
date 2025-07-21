5013a7e3eece15bd8f4f2d15d419768a7b6bce54
Moisés Brandão <moisesbrandao1998@gmail.com>
2025-07-21 02:20:13 -0300

    Create xray mod client

---
+++ src/client/java/xray/XRayClient.java
@@ -0,0 +1,29 @@
+package xray;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class XRayClient implements ClientModInitializer {
+
+    private static KeyBinding xRayKey = KeyBindingHelper.registerKeyBinding(
+            new KeyBinding("key.xray.toggle",
+                    InputUtil.Type.KEYSYM,
+                    GLFW.GLFW_KEY_X,
+                    "category.xray")
+    );
+
+	@Override
+	public void onInitializeClient() {
+        ClientTickEvents.END_CLIENT_TICK.register(
+                client -> {
+                    while (xRayKey.wasPressed()) {
+                        XRayManager.getInstance().toggle();
+                    }
+                }
+        );
+	}
+}
